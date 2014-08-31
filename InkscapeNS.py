6c470a51f4844088174837b5871d5ba734567c69
Steven Swanson <swanson@cs.ucsd.edu>
2014-08-31 06:38:23 +0000

    cleaned up the conventions for drawing svgs of components.  It's much easier now.  Added everything needed for mounting holes.  Fixed some bugs.  Automatically built a board and unetched faceplate from an svg file
    git-svn-id: svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/Tools/SVGUtil@18550 2cf53259-273a-af40-bda6-69d31023b8ec

---
+++ InkscapeNS.py
@@ -0,0 +1,4 @@
+
+namespaces = {'svg': 'http://www.w3.org/2000/svg',
+              'sodipodi': 'http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd',
+              'inkscape' : 'http://www.inkscape.org/namespaces/inkscape'}
